6f88b9b4fd59c236682e8d4762ecde8c575d6296
Aaron Schlesinger <arschles@gmail.com>
2011-05-28 00:49:50 -0700

    throwing if the redis key isn't found

--- src/main/scala/DataSource/Redis.scala
+++ src/main/scala/DataSource/Redis.scala
@@ -3,12 +3,20 @@ import scala.collection.immutable.HashMap
 import com.redis
 import com.redis._
 
+class NoSuchKeyException extends Exception
+{}
+
 class RedisDataSource(h: Host, key: String) extends DataSource
 {
 	var redis_ = new RedisClient(h.hostname, h.port)
 	var key_ = key
 		
 	def GetAll : Map[String, String] = {
-		redis_.hgetall(key_).getOrElse(null)
+		val res = redis_.hgetall(key_).getOrElse(null)
+		if(res == null)
+		{
+			throw new NoSuchKeyException
+		}
+		res
 	}
 }
