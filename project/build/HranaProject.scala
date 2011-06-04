2f60e2e1843385b3b952d14e83ae9e4554753412
Aaron Schlesinger <arschles@gmail.com>
2011-06-04 03:11:14 -0700

    moved deps into sbt auto dependency mgmt. saves space & is better

--- project/build/HranaProject.scala
+++ project/build/HranaProject.scala
@@ -8,4 +8,7 @@ class HranaProject(info: ProjectInfo) extends DefaultProject(info) with AutoComp
 	val mockito = "org.mockito" % "mockito-all" % "1.8.1"
 	val junit = "junit" % "junit" % "4.5"
 	val thrift = "org.apache.thrift" % "libthrift" % "0.6.1"
+	val gson = "com.google.gson" % "gson" % "1.7.1" from "http://google-gson.googlecode.com/files/google-gson-stream-1.7.1.jar"
+	val slf4j_api = "com.slf4j.api" % "slf4j-api" % "1.6.0" from "http://repo2.maven.org/maven2/org/slf4j/slf4j-api/1.6.0/slf4j-api-1.6.0.jar"
+	var slf4j_simple = "com.slf4j.simple" % "slf4j-simple" % "1.6.0" from "http://repo2.maven.org/maven2/org/slf4j/slf4j-simple/1.6.0/slf4j-simple-1.6.0.jar"
 }
